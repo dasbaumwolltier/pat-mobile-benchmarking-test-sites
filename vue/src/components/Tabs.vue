a4909ac3669ae3f5d2b06e8827f75a19ad9a3c6d
dasbaumwolltier <gabriel@guldner.eu>
2020-04-29 18:40:01 +0200

    fix switch tab

--- vue/src/components/Tabs.vue
+++ vue/src/components/Tabs.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <app-tab-button v-for="tab in tabs" :key="'tb-' + tab.id" :tabId="tab.id" v-on:activate="e => this.activeTab = e"></app-tab-button>
+    <app-tab-button v-for="tab in tabs" :key="'tb-' + tab.id" :tabId="tab.id" v-on:activate="e => activeTab = e"></app-tab-button>
     <app-tab-content v-for="tab in tabs" :key="'tc-' + tab.id" :tabId="tab.id" :activeTab="activeTab" :labelData="tab.data"></app-tab-content>
   </div>
 </template>
